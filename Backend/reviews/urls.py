6bb4397104bc9e54ee1f1cbc9ac901325918f29c
rahulkarkiii <rahulkarkeee@gmail.com>
2026-08-27 11:10:55 +0545

    Add review URLs

---
+++ Backend/reviews/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import (
+    ReviewListCreateView,
+    ReviewDetailView,
+)
+
+urlpatterns = [
+    path("", ReviewListCreateView.as_view(), name="review-list-create"),
+    path("<int:pk>/", ReviewDetailView.as_view(), name="review-detail"),
+]
