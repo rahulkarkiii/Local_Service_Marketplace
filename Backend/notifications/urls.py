fb0e03b1b7da2a92cc179924c7877c68530a163e
rahulkarkiii <rahulkarkeee@gmail.com>
2026-08-30 19:08:38 +0545

    Add notifications URLs

---
+++ Backend/notifications/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+from .views import (
+    NotificationListCreateView,
+    NotificationDetailView,
+)
+
+
+urlpatterns = [
+    path(
+        "",
+        NotificationListCreateView.as_view(),
+        name="notification-list-create",
+    ),
+    path(
+        "<int:pk>/",
+        NotificationDetailView.as_view(),
+        name="notification-detail",
+    ),
+]
