2d939cb22e5cd3cd1389195c2f95d41c59a594b7
rahulkarkiii <rahulkarkeee@gmail.com>
2026-08-30 19:08:16 +0545

    Update notifications views

--- Backend/notifications/views.py
+++ Backend/notifications/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Notification
+from .serializers import NotificationSerializer
+
+
+class NotificationListCreateView(generics.ListCreateAPIView):
+    serializer_class = NotificationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Notification.objects.filter(
+            recipient=self.request.user
+        )
+
+    def perform_create(self, serializer):
+        serializer.save(recipient=self.request.user)
+
+
+class NotificationDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = NotificationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Notification.objects.filter(
+            recipient=self.request.user
+        )
