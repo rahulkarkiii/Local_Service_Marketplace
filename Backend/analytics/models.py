0ac54f5587586c764cf44a9c6095d87244ce1c17
rahulkarkiii <rahulkarkeee@gmail.com>
2026-09-01 10:31:34 +0545

    Add analytics models and register admin

--- Backend/analytics/models.py
+++ Backend/analytics/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Analytics(models.Model):
+    date= models.DateField(unique=True)
+
+    total_users= models.PositiveIntegerField(default=0)
+    total_customers= models.PositiveIntegerField(default=0)
+    total_providers= models.PositiveIntegerField(default=0)
+    total_services= models.PositiveIntegerField(default=0)
+    total_bookings= models.PositiveIntegerField(default=0)
+    total_payments= models.PositiveIntegerField(default=0)
+    total_reports= models.PositiveIntegerField(default=0)
+
+    completed_bookings= models.PositiveIntegerField(default=0)
+    pending_bookings= models.PositiveIntegerField(default=0)
+
+    total_revenue= models.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+        default=0,
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"Analytics - {self.date}"
