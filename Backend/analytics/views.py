c193948157f95d0f1154850412eb189d5033282c
Rahul Karki <rahulkarkeee@gmail.com>
2026-09-01 11:19:35 +0545

    Rename analytics API views for consistency

--- Backend/analytics/views.py
+++ Backend/analytics/views.py
@@ -4,12 +4,12 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Analytics
 from.serializers import AnalyticsSerializer
 
-class AnalyticsListCreateAPIView(generics.ListCreateAPIView):
+class AnalyticsListCreateView(generics.ListCreateAPIView):
     queryset = Analytics.objects.all()
     serializer_class = AnalyticsSerializer
     permission_classes = [IsAuthenticated]
 
-class AnalyticsDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+class AnalyticsDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Analytics.objects.all()
     serializer_class = AnalyticsSerializer
     permission_classes = [IsAuthenticated]
