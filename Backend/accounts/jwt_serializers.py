28d092fc6588d3c65a891176d76861831f81f186
rahulkarkiii <rahulkarkeee@gmail.com>
2026-09-02 12:38:20 +0545

    Add JWT serializers

---
+++ Backend/accounts/jwt_serializers.py
@@ -0,0 +1,13 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from .models import Account
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token["user_id"] = user.id
+        token["username"] = user.username
+        token["role"] = user.role
+
+        return token
