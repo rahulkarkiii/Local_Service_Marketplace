6eb43795294eeee86970ff10b388f5547d8c5a95
rahulkarkiii <rahulkarkeee@gmail.com>
2026-09-02 12:37:31 +0545

    Update account URLs

--- Backend/accounts/urls.py
+++ Backend/accounts/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import RegisterView
+from .jwt_serializers import CustomTokenObtainPairSerializer
+from .views import RegisterView, MeView
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
 
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
-    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("login/", CustomTokenObtainPairView.as_view(), name="login"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("me/", MeView.as_view(), name="me"),
 ]
