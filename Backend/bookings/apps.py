173cbf3bbe0632c179ab03650eb4ee18fc60eb91
rahulkarkiii <rahulkarkeee@gmail.com>
2026-09-07 11:51:37 +0545

    feat(bookings): start APScheduler on app ready

--- Backend/bookings/apps.py
+++ Backend/bookings/apps.py
@@ -1,5 +1,18 @@
+import os
+import sys
+
 from django.apps import AppConfig
 
 
 class BookingsConfig(AppConfig):
     name = 'bookings'
+
+    def ready(self):
+        if 'runserver' not in sys.argv:
+            return
+
+        if os.environ.get('RUN_MAIN') != 'true':
+            return
+
+        from config import scheduler
+        scheduler.start()
